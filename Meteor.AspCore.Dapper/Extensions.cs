1f1e4351529f1d1dc4dc2cfc6628d58339d15298
Majid Hatami <hatami6570@gmail.com>
2020-12-21 12:41:47 +0330

    fix Extensions

--- Meteor.AspCore.Dapper/Extensions.cs
+++ Meteor.AspCore.Dapper/Extensions.cs
@@ -6,5 +6,15 @@ namespace Meteor.AspCore.Dapper
 {
     public static class Extensions
     {
+        public static IServiceCollection AddDbOperation<TDbConnectionFactory, TSqlDialect>(this IServiceCollection services)
+            where TDbConnectionFactory : class, IDbConnectionFactory
+            where TSqlDialect : ISqlDialect, new()
+        {
+            services.AddSingleton<IDbConnectionFactory, TDbConnectionFactory>();
+            services.AddSingleton<ISqlFactory, SqlFactory<TSqlDialect>>();
+            services.AddScoped<LazyDbConnection>();
+
+            return services;
+        }
     }
 }
